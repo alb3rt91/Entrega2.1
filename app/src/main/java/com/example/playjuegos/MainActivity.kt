43395831a520fd5349efdf63252c16539db1c5ba
albert <albeert_91@yahoo.es>
2024-10-16 11:59:26 +0200

    Initial commit

---
+++ app/src/main/java/com/example/playjuegos/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.example.playjuegos
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Button
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.appcompat.app.AppCompatActivity
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val jugador = findViewById(R.id.button1) as Button
+
+        jugador.setOnClickListener { lanzarNewplayer() }
+    }
+
+    private fun lanzarNewplayer() {
+        val intent = Intent(this, Newplayer::class.java)
+        startActivity(intent)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val id = item.getItemId()
+
+        if (id==R.id.action_buscar) {
+            Toast.makeText(this, "Búsqueda",Toast.LENGTH_LONG).show();
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
+}
---
+++ app/src/main/java/com/example/playjuegos/Newplayer.kt
@@ -0,0 +1,12 @@
+package com.example.playjuegos
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+
+class Newplayer : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.newplayer)
+    }
+}
